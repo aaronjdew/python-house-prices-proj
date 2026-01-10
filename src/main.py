0644be82ada4b5794ce4798ef5297c722ebae83f
aaronjdew <aaronjames8us@gmail.com>
2026-01-09 16:18:01 -0800

    creation of machine learning model

--- src/main.py
+++ src/main.py
@@ -2,9 +2,13 @@
     MAIN PROCEDURE
         -- Extract all data from source files and merge
         -- Prepare data for machine learning
+        -- Create machine learning model
 -------------------------------------------------------"""
 from extract_all_data import extract_src_data
 from prep_data import prep_data
+from ml_model import ml_model, model_diags
+
 
 df_src_data = extract_src_data()
 df_prp_data = prep_data(df_src_data)
+preds, accuracy, target = ml_model(df_prp_data, False)
